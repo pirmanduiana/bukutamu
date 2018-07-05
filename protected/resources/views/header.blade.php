27643b21dc7526ede17ff2a9e33dcb293f447a7a
Aditya Setiawan <aditya.setiawan21.as@gmail.com>
2018-07-05 11:34:01 +0800

    rubah layout

--- protected/resources/views/header.blade.php
+++ protected/resources/views/header.blade.php
@@ -2,6 +2,5 @@
     <div class="sites-logo">
         <img src="assets/images/logo-pemkab.png" alt="logo-pemkab-badung">
     </div>
-    <h1>Nama Pameran</h1>
-    <p>Dinas Komunikasi dan Informasi Kabupaten Badung</p>
+    <p>Diskominfo Kabupaten Badung</p>
 </div>
