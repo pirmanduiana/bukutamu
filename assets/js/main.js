2b9f89631e69388b3fb19db773cf85d457f839e5
Lely Meilina <lelymeilina452@gmail.com>
2018-07-05 11:39:08 +0800

    main js

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+ $(document).ready(function(){  
+
+  $(document).on('submit', '#tambah', function() {
+
+        $.confirm({
+            columnClass: 'col-md-6 col-md-offset-3',
+            icon: 'fa fa-user',
+            title: 'Berhasil Menyimpan Data',
+            content: 'Terima Kasih Atas Partisipasi Anda.',
+            autoClose: 'tutup|10000',
+            type: 'dark',
+            typeAnimated: true,
+            buttons: {
+                tutup: function () {
+                  text: 'Tutup'
+                }
+            }
+
+        });
+
+  });
+
+
+
+
+
+});//end doc ready //end doc ready //end doc ready //end doc ready //end doc ready //end doc ready
+
+
+
+
+
+
+
