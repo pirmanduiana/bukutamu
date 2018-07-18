bcef13d9372a3d4a69d90b422763a25276071009
Aditya Setiawan <aditya.setiawan21.as@gmail.com>
2018-07-18 11:57:27 +0800

    Centering captions

--- protected/resources/views/tabs-feedback.blade.php
+++ protected/resources/views/tabs-feedback.blade.php
@@ -23,24 +23,18 @@
             <li class="active question"><a href="javascript:void(0)">{{config('app.question')}}</a></li>
         </ul>
         <div class="tab-content">
-            <div id="bukutamu" class="tab-pane fade">
-                
-            </div>
             <div id="feedback" class="tab-pane fade in active">
                 <div align="center">
                     
                     {{ HTML::image('assets/images/smile_face.png', 'Smile', ['id'=>'vote_smile','class'=>'vote_icons','width'=>'250px','height'=>'250px']) }}
 
                     {{ HTML::image('assets/images/flat_face.png', 'Flat', ['id'=>'vote_flat','class'=>'vote_icons','width'=>'250px','height'=>'250px']) }}
-                    
-           
-
-                    
+                
                 </div>
-
-                        <span style="margin: 0px 0px 0px 230px; font-size: 25px; color:orange"> <b>PUAS</b> </span>
-                        <span style="margin: 0px 0px 0px 300px; font-size: 25px; color:orange"> <b>CUKUP</b> </span>
-                 
+                <div class="captions">
+                    <span> <b>PUAS</b> </span>
+                    <span> <b>CUKUP</b> </span>
+                </div>
             </div>
         </div>
     </div>
